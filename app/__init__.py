220b5c500a7c713447092185820d41cbeb97b54a
Braxton Quintero <hiitsbraxton@gmail.com>
2022-11-18 22:21:53 -0700

    got signup working

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,14 @@ from config import Config
 app = Flask(__name__)
 app.config.from_object(Config)
 
+db=SQLAlchemy(app)
+
+migrate=Migrate(app,db)
+
+
+
+login = LoginManager(app)
+
+login.login_view = 'login'
+
 from . import routes
---
+++ app/models.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import db, login
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True) 
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(256), nullable=False)
+    date_created = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        
+        self.password = self.set_password(kwargs.get('password', ''))
+
+        db.session.add(self)
+        db.session.commit()
+
+    def __str__(self):
+        return self.username
+
+    def set_password(self, plain_password):
+        return generate_password_hash(plain_password)
+
+    def check_password(self, password_guess):
+        return check_password_hash(self.password, password_guess)
+
+
+@login.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)

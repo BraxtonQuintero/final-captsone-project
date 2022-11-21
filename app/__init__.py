6254bb10194798b121600f5b4ff2ccc9c9b30f73
Braxton Quintero <hiitsbraxton@gmail.com>
2022-11-21 13:29:18 -0700

    changed website colors

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate 
 from flask_login import LoginManager
 from config import Config
-from cod_api import API
 
 app = Flask(__name__)
-
-api = API()
-api.login('MzEyNjA0MTUyNjM5ODQzOTk0OToxNjcwMjY3Njk1NzA2Ojc0Yjg2OTcwNzA4ZmRjMzA5ZWQ3MGRjMjkxN2M3NDJk')
 
 app.config.from_object(Config)
 
